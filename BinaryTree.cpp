209031d82d4e367236a60cda38a9985280a8fcd6
mohamed elafandy <mohamedelafandy16@gmail.com>
2025-10-06 19:53:33 +0300

    QueueCallingInBinaryTree

--- BinaryTree.cpp
+++ BinaryTree.cpp
@@ -1,86 +1,10 @@
 #include<iostream>
-#include<./queue>
+#include<./QueueByArray>
 #include <algorithm>
 #include <cmath>
 #include <stdexcept>
+
 using namespace std;
-template<class T>
-class Queue {
-    T* arr;
-    int capacity;
-    int topIndex;
-	int zeroIndex;
-    //length = topIndex + 1;
-
-public:
-    Queue(int initialCapacity = 10) {
-        arr = new T[initialCapacity];
-        capacity = initialCapacity;
-        topIndex = -1;
-        zeroIndex = 0;
-    }
-
-    void resize() {
-        capacity *= 2;
-        T* newArr = new T[capacity];
-		int size = this->getSize();
-        for (int i = 0; i < size; i++) {
-            newArr[i] = arr[zeroIndex+i];
-        }
-        //std:copy(arr, arr + topIndex + 1, newArr);
-
-        delete[] arr;
-        arr = newArr;
-
-        topIndex = size - 1;
-        zeroIndex = 0;
-
-    }
-
-    void enqueue(T value) {
-        if (topIndex + 1 == capacity) {
-            resize();
-        }
-        arr[++topIndex] = value;
-    }
-
-    T dequeue() {
-        if (isEmpty()) {
-            throw runtime_error("Queue is empty");
-        }
-        T temp = arr[zeroIndex];
-        arr[zeroIndex] = T();
-        ++zeroIndex;
-        return temp;
-    }
-
-    T peek() {
-        if (isEmpty()) {
-            throw runtime_error("Queue is empty");
-        }
-        return arr[zeroIndex];
-    }
-
-    bool isEmpty() {
-        return zeroIndex > topIndex;
-    }
-    int getSize() {
-        if (isEmpty()) return 0;
-        return topIndex - zeroIndex + 1;
-    }
-
-    void print() {
-        for (int i = zeroIndex; i <= topIndex; i++) {
-            cout << arr[i] << " ";
-        }
-        cout << endl;
-    }
-
-    ~Queue() {
-        delete[] arr;
-    }
-};
-
 template <class T>
 class TreeNode{
 	public:
